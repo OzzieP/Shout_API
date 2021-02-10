a8464fc3868f738f8e689839332e165b47bc4727
Cahier <francois.cahier@worldline.com>
2021-02-10 16:28:54 +0100

    Post

---
+++ Shout_API/Controllers/ShoutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shout_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shout_API.Controllers
+{
+    [Route("api/shout")]
+    [ApiController]
+    public class ShoutController : ControllerBase
+    {
+        // POST api/shout
+        [HttpPost]
+        public string Post([FromBody] Chaine chaine)
+        {
+            return chaine.Libelle;
+        }
+    }
+}
---
+++ Shout_API/Models/Chaine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shout_API.Models
+{
+    public class Chaine
+    {
+        private String libelle;
+
+        public String Libelle
+        {
+            get { return libelle; }
+            set { libelle = value; }
+        }
+
+    }
+}
