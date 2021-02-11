2d1502e765bdf5527e2f2833e7b5efd7dbb2700a
matthieu <matthieu.dubois@epsi.fr>
2021-02-11 09:17:40 +0100

    TestProject

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            int a = 5;
+            int b = 10;
+
+            Assert.Equal(15, a + b);
+
+
+        }
+    }
+}
